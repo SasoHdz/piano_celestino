838ed5257443e12802445339b709d41a8e995c45
SasoHdz <isaacrdzcmd@gmail.com>
2023-02-16 16:23:02 -0600

    Proyecto piano listo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,46 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'piano';
-}
+
+  aplicarSonido(n : number) {
+    let audio = new Audio();
+    switch (n){
+      case 1:
+        audio.src='../assets/sonidos/do.mp3';
+        audio.load();
+        audio.play();
+        break;
+      case 2:
+        audio.src='../assets/sonidos/re.mp3';
+        audio.load();
+        audio.play();
+        break;
+      case 3:
+        audio.src='../assets/sonidos/mi.mp3';
+        audio.load();
+        audio.play();
+        break;
+      case 4:
+        audio.src='../assets/sonidos/fa.mp3';
+        audio.load();
+        audio.play();
+        break;
+      case 5:
+        audio.src='../assets/sonidos/sol.mp3';
+        audio.load();
+        audio.play();
+        break;
+      case 6:
+        audio.src='../assets/sonidos/la.mp3';
+        audio.load();
+        audio.play();
+        break;
+      case 7:
+        audio.src='../assets/sonidos/si.mp3';
+        audio.load();
+        audio.play();
+        break;
+      }
+    }
+  }
+
